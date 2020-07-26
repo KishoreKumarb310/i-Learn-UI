82b40393056f91e6d2a5233ddb9a34d77c91fd81
KishoreKumarb310 <kishore.kiku310@gmail.com>
2020-07-26 21:55:19 +0530

    Updates

---
+++ precache-manifest.9b8dcd0362257ad2da82daaa7391f0bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5d82decd3a42b18ca7a40316e6394e",
+    "url": "https://kishorekumarb310.github.io/iLearn/index.html"
+  },
+  {
+    "revision": "018c22f4a47d0f710657",
+    "url": "https://kishorekumarb310.github.io/iLearn/static/js/2.4e12dd4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://kishorekumarb310.github.io/iLearn/static/js/2.4e12dd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f71b4c4ef5727ffb2346",
+    "url": "https://kishorekumarb310.github.io/iLearn/static/js/main.b4d19e58.chunk.js"
+  },
+  {
+    "revision": "d0149ec5aadbb27dd5ce",
+    "url": "https://kishorekumarb310.github.io/iLearn/static/js/runtime-main.10bbca5d.js"
+  }
+]);
