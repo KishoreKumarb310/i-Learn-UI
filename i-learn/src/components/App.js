cd32ca35844de9cabb40ab046f467a33ace5552e
KishoreKumarb310 <kishore.kiku310@gmail.com>
2020-07-26 22:12:37 +0530

    Added css file

--- i-learn/src/components/App.js
+++ i-learn/src/components/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
+import '../styles/index.css';
 
 class App extends Component {
 
     render(){
         return(
-            <div>
-                <h2>i-Learn</h2>
+            <div className="App">
+                <header className="title">i-Learn</header>
+                <div className="wrapper">
+
+                </div>
+                <footer className="footer">&copy;kiku_corps</footer>
             </div>
         )
     }
